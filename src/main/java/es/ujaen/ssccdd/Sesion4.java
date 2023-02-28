44431db035e0d86a14045f20944cf5ccb3dc8eb5
manuel <mjmp0027@red.ujaen.es>
2023-02-28 16:32:22 +0100

    Guión Sesión 4 - Ejemplo 3

--- src/main/java/es/ujaen/ssccdd/Sesion4.java
+++ src/main/java/es/ujaen/ssccdd/Sesion4.java
@@ -1,13 +1,44 @@
 package es.ujaen.ssccdd;
 
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.concurrent.*;
+
 public class Sesion4 {
 
     public static void main(String[] args) {
-        Server server = new Server();
-        for (int i = 0; i < 50; i++) {
-            Task task = new Task("Task " + i);
-            server.executeTask(task);
+        ThreadPoolExecutor executor = new ThreadPoolExecutor(2, 2, 0, TimeUnit.MICROSECONDS, new LinkedBlockingQueue<>(10));
+        ArrayList<Future<Integer>> resultList = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            Random random = new Random();
+            Integer numero = random.nextInt(10);
+            FactorialCalculator calculator = new FactorialCalculator(numero);
+            Future<Integer> result = executor.submit(calculator);
+            resultList.add(result);
         }
-        server.endServer();
+        do {
+            System.out.println("Main: Number of Completed tasks: " + executor.getCompletedTaskCount());
+            for (int i = 0; i < resultList.size(); i++) {
+                Future<Integer> result = resultList.get(i);
+                System.out.println("Main: Task " + i + ": " + result.isDone());
+            }
+            try{
+                Thread.sleep(50);
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        } while (executor.getCompletedTaskCount() < resultList.size());
+        System.out.println("Main results: \n");
+        for (int i = 0; i < resultList.size(); i++) {
+            Future<Integer> result = resultList.get(i);
+            Integer numero = null;
+            try{
+                numero = result.get();
+            }catch (InterruptedException | ExecutionException e){
+                e.printStackTrace();
+            }
+            System.out.println("Core: Task "+ i + ": " + numero);
+        }
+        executor.shutdown();
     }
 }
