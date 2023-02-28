764c84002a48ae8fbf6b7354d1cdaa074d4a0ad7
manuel <mjmp0027@red.ujaen.es>
2023-02-28 13:21:27 +0100

    Guión Sesión 4 - Ejemplo 2

--- src/main/java/es/ujaen/ssccdd/Server.java
+++ src/main/java/es/ujaen/ssccdd/Server.java
@@ -1,8 +1,6 @@
 package es.ujaen.ssccdd;
 
-import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 
 public class Server {
     ThreadPoolExecutor hiloEjecutor;
@@ -11,6 +9,7 @@ public class Server {
         int poolSize = 10;
         int queueSize = 50;
         hiloEjecutor = new ThreadPoolExecutor(poolSize, poolSize, 0L, TimeUnit.MILLISECONDS, new LinkedBlockingQueue<>(queueSize));
+        hiloEjecutor = (ThreadPoolExecutor) Executors.newFixedThreadPool(5);
     }
 
     public void executeTask(Task task){
@@ -19,6 +18,7 @@ public class Server {
         System.out.printf("Server: Pool Size: %d\n",hiloEjecutor.getPoolSize());
         System.out.printf("Server: Active Count: %d\n",hiloEjecutor.getActiveCount());
         System.out.printf("Server: Completed Tasks: %d\n",hiloEjecutor.getCompletedTaskCount());
+        System.out.printf("Server: Task Count: %d\n",hiloEjecutor.getTaskCount());
     }
 
     public void endServer(){
