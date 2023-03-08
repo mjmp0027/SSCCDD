d69890b9754efd2bda04c1c8c82bdc123d70946a
Mangue8 <manueljesusmoralesperez@gmail.com>
2023-03-08 18:13:24 +0100

    Guión Sesión 5 - Ejemplo 6

--- src/main/java/es/ujaen/ssccdd/Sesion5.java
+++ src/main/java/es/ujaen/ssccdd/Sesion5.java
@@ -6,40 +6,23 @@ import java.util.concurrent.*;
 public class Sesion5 {
 
     public static void main(String[] args) throws RuntimeException {
-        ExecutorService executor = Executors.newCachedThreadPool();
-        CompletionService<String> service = new ExecutorCompletionService<>(executor);
 
-        ReportRequest faceRequest = new ReportRequest("Face", service);
-        ReportRequest onlineRequest = new ReportRequest("Online", service);
-        Thread faceThread = new Thread(faceRequest);
-        Thread onlineThread = new Thread(onlineRequest);
+        RejectedExecutionHandler controller = new RejectedTaskController();
+        ThreadPoolExecutor executor = (ThreadPoolExecutor)Executors.newCachedThreadPool();
+        executor.setRejectedExecutionHandler(controller);
 
-        ReportProcessor processor = new ReportProcessor(service);
-        Thread senderThread = new Thread(processor);
-
-        System.out.println("Main starting the Threads");
-        faceThread.start();
-        onlineThread.start();
-        senderThread.start();
-
-        try {
-            System.out.print("Main waiting for the request generators.\n");
-            faceThread.join();
-            onlineThread.join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        System.out.println("Main starting\n");
+        for (int i = 0; i < 3; i++) {
+            Task task = new Task("Task " + i);
+            executor.submit(task);
         }
 
-        System.out.println("Main shuting down the executor.\n");
+        System.out.println("Main shutting down the executor\n");
         executor.shutdown();
 
-        try {
-            executor.awaitTermination(1, TimeUnit.DAYS);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        processor.setEnd(true);
+        System.out.print("Main: Sending another Task.\n");
+        Task task=new Task("RejectedTask");
+        executor.submit(task);
 
         System.out.println("Main ends at: " + new Date());
     }
--- src/main/java/es/ujaen/ssccdd/Task.java
+++ src/main/java/es/ujaen/ssccdd/Task.java
@@ -1,18 +1,25 @@
 package es.ujaen.ssccdd;
 
-import java.util.concurrent.Callable;
+import java.util.concurrent.TimeUnit;
 
-public class Task implements Callable<String> {
+public class Task implements Runnable {
 
-    public Task() {
+    private final String name;
 
+    public Task(String name) {
+        this.name = name;
     }
 
     @Override
-    public String call() throws Exception {
-        while (true) {
-            System.out.print("Task: Test\n");
-            Thread.sleep(100);
+    public void run() {
+        System.out.println("Task: " + name + " starting\n");
+        try {
+            long duration = (long) (Math.random() * 10);
+            System.out.println("Task: " + name + " generating a report during " + duration + " seconds\n");
+            TimeUnit.SECONDS.sleep(duration);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
+        System.out.println("Task: " + name + " ending\n");
     }
 }
