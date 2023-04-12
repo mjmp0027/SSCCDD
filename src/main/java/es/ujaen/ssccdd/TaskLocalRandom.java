62cb64b253ccce406aaef8603d3fa61655ccf475
Mangue82 <manuel.morales@videocaranalytics.es>
2023-04-12 15:39:48 +0200

    Guión Sesión 9 - Ejemplo 2

---
+++ src/main/java/es/ujaen/ssccdd/TaskLocalRandom.java
@@ -0,0 +1,16 @@
+package es.ujaen.ssccdd;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+public class TaskLocalRandom implements Runnable {
+    public TaskLocalRandom() {
+        ThreadLocalRandom.current();
+    }
+
+    @Override
+    public void run() {
+        String name = Thread.currentThread().getName();
+        for (int i = 0; i < 10; i++)
+            System.out.printf("%s: %d\n", name, ThreadLocalRandom.current().nextInt(10));
+    }
+}
